0e3d7ef8fcab1f5dd26e719b50d4045ee6319e9f
Bryce Kwon <signer@brycekwon.com>
2024-12-11 19:28:47 -0800

    update: add coursework for up-cs305

---
+++ university-of-portland/cs305/hw02/run_tests.sh
@@ -0,0 +1,30 @@
+echo "Test Board 1"
+./find_path board1.txt
+echo "Test Board 2"
+./find_path board2.txt
+echo "Test Board 3"
+./find_path board3.txt
+echo "Test Board 4"
+./find_path board4.txt
+echo "Test Board 5"
+./find_path board5.txt
+echo "Test Board 6"
+./find_path board6.txt
+echo "Test Board 7"
+./find_path board7.txt
+echo "Test Board 8"
+./find_path board8.txt
+echo "Test Board 9"
+./find_path board9.txt
+echo "Test Board 10"
+./find_path board10.txt
+echo "Test Board 11"
+./find_path board11.txt
+echo "Test Board 12"
+./find_path board12.txt
+echo "Test Board 13"
+./find_path board13.txt
+echo "Test Board 14"
+./find_path board14.txt
+echo "Test Board 15"
+./find_path board15.txt
